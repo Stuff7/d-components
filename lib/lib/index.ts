3b4d387f23da6a45efce47a382230d50cff5be44
Armando M <cuentafrst@gmail.com>
2023-07-15 20:55:57 -0600

    add default export

--- lib/lib/index.ts
+++ lib/lib/index.ts
@@ -3,3 +3,4 @@ export { default as DIcon } from "~/components/DIcon.vue";
 export { default as DInput } from "~/components/DInput.vue";
 export { default as DSelect } from "~/components/DSelect.vue";
 export { default as DSwitch } from "~/components/DSwitch.vue";
+export { default as DTooltip } from "~/components/DTooltip.vue";
