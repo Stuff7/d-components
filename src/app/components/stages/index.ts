aee59335757239f2a35f0d015ea1739f99d61deb
Armando <cuentafrst@gmail.com>
2023-07-28 04:41:15 +0000

    Build static site.

--- src/app/components/stages/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as DInput } from "@app/components/stages/DInput.vue";
-export { default as DIcon } from "@app/components/stages/DIcon.vue";
-export { default as DSwitch } from "@app/components/stages/DSwitch.vue";
-export { default as DTooltip } from "@app/components/stages/DTooltip.vue";
-export { default as DSelect } from "@app/components/stages/DSelect.vue";
-export { default as DFoldable } from "@app/components/stages/DFoldable.vue";
